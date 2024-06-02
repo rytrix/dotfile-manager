28534da7d03766b012f8ed93e477c6a4ae071928
Ryan <rmw071102@gmail.com>
2024-06-02 16:25:33 -0400

    dry run

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ struct Args {
     #[arg(long, value_name = "boolean", action = ArgAction::SetTrue)]
     list_full: bool,
 
-    /// Dryrun
+    /// Dry run
     #[arg(short = 'r', long, value_name = "boolean", action = ArgAction::SetTrue)]
     dry_run: bool,
 }
